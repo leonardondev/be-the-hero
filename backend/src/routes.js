cb8ba1dd2b957bb919120b32d5043790e6ec709a
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2020-03-25 18:06:56 -0300

    rota de inserir ongs implementada

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const crypto = require('crypto');
+
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.post('/users', (request, response) => {
-  return response.json({ mensage: 'Omnistack11'});
+routes.post('/ongs', async (request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body;
+
+  const id = crypto.randomBytes(4).toString('HEX'); //gera id
+  
+  await connection('ongs').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf,
+  });
+  
+  return response.json({ id });
 });
 
 module.exports = routes;

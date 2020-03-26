397a191b6e3ed9ec06d28ace749c8edefe3e28a9
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2020-03-25 22:42:56 -0300

    criacao da rota de index para casos de uma unica ONG

---
+++ backend/src/controllers/ProfileController.js
@@ -0,0 +1,30 @@
+const connection = require('../database/connection');
+
+module.exports = {
+   async index(request,response) {
+    const ong_id = request.headers.authorization;
+
+    const incidents = await connection('incidents')
+      .where('ong_id', ong_id)
+      .select('*');
+
+    return response.json(incidents);
+   },
+   
+   async show(request,response) {
+   
+   },
+   
+   async store(request,response) {
+   
+   },
+   
+   async update(request,response) {
+   
+   },
+   
+   async destroy(request,response) {
+   
+   },
+   
+}

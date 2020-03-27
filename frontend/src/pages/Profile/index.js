fb31b8f3a33e540f72d62e78122677a0439288e4
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2020-03-27 11:17:08 -0300

    botão deletar casos implementado

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -25,6 +25,20 @@ export default function Profile() {
      })
   },[ongId]);
 
+  async function handleDeleteIncident(id) {
+    try {
+      await api.delete(`/incidents/${id}`, {
+        headers: {
+          Authorization: ongId,
+        }
+      });
+
+      setIncidents(incidents.filter(incident => incident.id !== id));
+    } catch (err) {
+      alert('Erro ao deletar caso, tente novamente');
+    }
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -51,7 +65,7 @@ export default function Profile() {
             <strong>VALOR:</strong>
             <p>{ Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(incident.value) }</p>
 
-            <button type="buton" >
+            <button onClick={() => handleDeleteIncident(incident.id)} type="buton" >
               <FiTrash2 size={20} color="#a8a8b3"/>
             </button>
           </li>

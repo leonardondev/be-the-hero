c31f1f076f444d4ad6c14bf8cc57f4a5e86e5627
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2020-03-27 11:45:34 -0300

    botao logout e novo caso implementado

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -11,6 +11,7 @@ import logoImg from '../../assets/logo.svg';
 export default function Profile() {
   const [incidents, setIncidents] = useState([]);
   
+  const history = useHistory();
 
   const ongId = localStorage.getItem('ongId');
   const ongName = localStorage.getItem('ongName');
@@ -39,6 +40,11 @@ export default function Profile() {
     }
   }
 
+  function handleLogout() {
+    localStorage.clear();
+    history.push('/');
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -46,7 +52,7 @@ export default function Profile() {
         <span>Bem vinda, {ongName}</span>
 
         <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-        <button type="buton" >
+        <button onClick={handleLogout} type="buton" >
           <FiPower size={18} color="#E02041"/>
         </button>
       </header>

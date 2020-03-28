1c2adcdd6813e5504b9bd38ed62eadb356e8770f
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2020-03-28 19:08:55 -0300

    paginacao infinita adicionada.

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -12,6 +12,10 @@ import styles from './styles';
 export default function Incidents() {
   const [incidents,setIncidents] = useState([]);
   const [total,setTotal] = useState(0);
+  const [page,setPage] = useState(1);
+  const [loading,setLoading] = useState(false);
+  
+  
   
   const navigation = useNavigation();
 
@@ -20,10 +24,22 @@ export default function Incidents() {
   }
 
   async function loadIncidents() {
-    const response = await api.get('incidents');
+    if (loading) {
+      return;
+    }
 
-    setIncidents(response.data);
+    if (total >0 && incidents.length === total) {
+      return;
+    }
+
+    setLoading(true);
+    const response = await api.get('incidents', { params: { page } });
+
+    setIncidents([ ...incidents, ...response.data ]);
     setTotal(response.headers['x-total-count']);
+
+    setPage(page +1);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -48,6 +64,8 @@ export default function Incidents() {
         style={styles.incidentList}
         keyExtractor={i => String(i.id)}
         showsVerticalScrollIndicator={false}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
         renderItem={({ item: i }) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>ONG:</Text>

6bfacec4c243ec1c6aa14f1e445ecd7ed14d8b8a
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2020-03-27 23:59:59 -0300

    Cabecalho da Rota Incidents feito

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import logoImg from './../../assets/logo.png';
+
+import styles from './styles';
+
 export default function Detail() {
   return (
-    <View />
+    <View style={styles.container}></View>
   );
 }
